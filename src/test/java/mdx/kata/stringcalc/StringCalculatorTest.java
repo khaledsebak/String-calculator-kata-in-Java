74323f909d29b5eaff66fdcda57dd4cde7f7ca53
Marcin Deryło <marcinderylo@gmail.com>
2011-11-05 19:37:08 +0100

    Third test failing

--- src/test/java/mdx/kata/stringcalc/StringCalculatorTest.java
+++ src/test/java/mdx/kata/stringcalc/StringCalculatorTest.java
@@ -12,17 +12,22 @@ public class StringCalculatorTest {
     @Test
     public void givenEmptyStringReturnsZero() throws Exception {
         String emptyString = "";
-        int expectedResult = 0;
-        assertResultForGivenInputStringIs(emptyString, expectedResult);
+        assertResultForGivenInputStringIs(emptyString, 0);
     }
 
     @Test
     public void givenSingleNumberReturnsItsValue() throws Exception {
         String singleNumber = "123";
-        int expectedResult = 123;
-        assertResultForGivenInputStringIs(singleNumber, expectedResult);
+        int valueOfTheNumber = 123;
+        assertResultForGivenInputStringIs(singleNumber, valueOfTheNumber);
     }
 
+    @Test
+    public void givenPairOfComaSeparatedNumbersReturnTheirSum() throws Exception {
+        String pairOfNumbers = "5,13";
+        int sumOfNumbers = 5 + 13;
+        assertResultForGivenInputStringIs(pairOfNumbers, sumOfNumbers);
+    }
 
     private void assertResultForGivenInputStringIs(String inputString, int expectedResult) {
         int actualResult = new StringCalculator().add(inputString);
