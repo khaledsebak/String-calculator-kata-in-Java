f85f5d845a7b9b9ba7e3c2f809b62bbf5b1a542a
Marcin Deryło <marcinderylo@gmail.com>
2011-11-05 19:43:58 +0100

    Third test passing

--- src/main/java/mdx/kata/stringcalc/StringCalculator.java
+++ src/main/java/mdx/kata/stringcalc/StringCalculator.java
@@ -2,14 +2,25 @@ package mdx.kata.stringcalc;
 
 import com.google.common.base.Strings;
 
+import static java.lang.Integer.valueOf;
+
 /**
  * @author Marcin Deryło <marcinderylo@gmail.com>
  */
 public class StringCalculator {
+    /**
+     * @param numbers coma-separated numbers
+     * @return sum of numbers (1 or 2) contained in the input string (<code>0</code> for empty string)
+     */
     public int add(String numbers) {
         if (Strings.isNullOrEmpty(numbers)) {
             return 0;
         }
-        return Integer.valueOf(numbers);
+        if (numbers.contains(",")) {
+            String[] bothNumbers = numbers.split(",");
+            return valueOf(bothNumbers[0]) + valueOf(bothNumbers[1]);
+
+        }
+        return valueOf(numbers);
     }
 }
